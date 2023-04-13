dc88df78c249f29af4ee139652ffd78302ea6f1d
theogodd <83649721+theogodd@users.noreply.github.com>
2023-04-13 18:21:42 +0100

    functions move / test w short vid
    beginning to try to understand why i get the 'video element has not loaded data yet, please wait for 'loadeddata' event on the <video> element' ERROR

--- sketch.js
+++ sketch.js
@@ -17,7 +17,6 @@ function setup(){
   createCanvas(1280, 720);
   // choose the correct input
   still_input = 'scene2outfit1';
-  console.log(still_input);
   // load in the right JSON file with the information about all of the stills
   stillCoordsJSON = loadJSON(`assets/JsonAndDat_${still_input}_2.json`);
   // preload the stills before the video
@@ -27,13 +26,15 @@ function setup(){
   video_scene1outfit1.hide();
   video_scene1outfit2 = createVideo("assets/scene1outfit2.mp4", loadVideo);
   video_scene1outfit2.hide();
+  video_scene1outfit2_short = createVideo("assets/scene1outfit2_short.mp4", loadVideo);
+  video_scene1outfit2.hide();
   video_scene2outfit1 = createVideo("assets/scene2outfit1.mp4", loadVideo);
   video_scene2outfit1.hide();
   video_scene2outfit2 = createVideo("assets/scene2outfit2.mp4", loadVideo);
   video_scene2outfit2.hide();
 
   // choose video
-  video_input = video_scene2outfit2;
+  video_input = video_scene1outfit2_short;
   video_input.size(width, height);
   frameRate(14);
 
@@ -57,6 +58,85 @@ function draw(){
 
     drawData();
   }
+}
+
+function loadVideo(){
+  linkPoseNet();
+  video_input.hide();
+  videoIsPlaying = false;
+}
+
+function linkPoseNet(stream){
+    // Create a new poseNet method with a single detection
+    poseNet = ml5.poseNet(video_input, poseNetLoaded);
+    // .on() is an event listener
+    poseNet.on('pose', detectedPose);
+}
+
+function poseNetLoaded(){
+  console.log("PoseNet iz Loaded!");
+}
+
+function loadStills(){
+  // change depending on input folder
+  // scene1outfit1 = 2592
+  // scene2outfit2 = 3192
+  var amountOfStills = 3762;
+  for(i = 1; i < amountOfStills; i++){
+    // imgName give the images in StillImages a name to be referenced
+    var imgName = `${still_input}_${('0000'+i).slice(-4)}`;
+    // load in the correct still with reference to leading zeros
+    // still input one because its the same as still_input but needs to be different from the string that you defined before
+    var still_input1 = createImg(`assets/${still_input}_output3/${still_input}_${('0000'+i).slice(-4)}.jpg`);
+    // push to array 
+    StillImages.push(still_input1);
+    StillImages[imgName] = still_input1;
+    still_input1.hide();
+  }
+  console.log(still_input, "- stills loaded in");
+}
+
+// control the playing of the video
+function keyPressed(){
+  // p for PAUSE / PLAY
+  if(key == 'p'){
+    if(videoIsPlaying){
+      videoIsPlaying = false;
+      video_input.pause();
+    }
+    else{
+      videoIsPlaying = true;
+      // call link PoseNet here so that it is relinked on the loop and the code can run over and over
+      linkPoseNet();
+      video_input.loop();
+    } 
+  }
+  if(key == 'd' && drawKeypoints_toggle == false){
+    drawKeypoints_toggle = true;
+  }
+  else if(key == 'd' && drawKeypoints_toggle == true){
+    drawKeypoints_toggle = false;
+  }
+
+  // adjust threshold
+  if(keyCode == LEFT_ARROW){
+    threshold -= 100;
+  }
+  if(keyCode == RIGHT_ARROW){
+    threshold += 100;
+  }
+
+  // fullscreen mode f
+  if( key == 'f'){
+    let fs = fullscreen();
+    fullscreen(!fs);
+  }
+}
+
+function detectedPose(results){
+  // we store the pose in the poses global variable
+  poses = results;
+  
 }
 
 function drawData(){
@@ -115,94 +195,6 @@ function drawData(){
     text((minimumVal), 5, 125);
   } 
   pop();
-}
-
-function linkPoseNet(stream){
-    // Create a new poseNet method with a single detection
-    poseNet = ml5.poseNet(video_input, poseNetLoaded);
-    // .on() is an event listener
-    poseNet.on('pose', detectedPose);
-}
-
-function poseNetLoaded(){
-  console.log("poseNet iz Loaded!");
-}
-
-function loadVideo(){
-  linkPoseNet();
-  video_input.hide();
-  videoIsPlaying = false;
-}
-
-function loadStills(){
-  // change depending on input folder
-  // scene1outfit1 = 2592
-  // scene2outfit2 = 3192
-  var amountOfStills = 3762;
-  for(i = 1; i < amountOfStills; i++){
-    // imgName give the images in StillImages a name to be referenced
-    var imgName = `${still_input}_${('0000'+i).slice(-4)}`;
-    // load in the correct still with reference to leading zeros
-    // still input one because its the same as still_input but needs to be different from the string that you defined before
-    var still_input1 = createImg(`assets/${still_input}_output3/${still_input}_${('0000'+i).slice(-4)}.jpg`);
-    // push to array 
-    StillImages.push(still_input1);
-    StillImages[imgName] = still_input1;
-    still_input1.hide();
-  }
-  console.log("stills loaded in");
-}
-
-// control the playing of the video
-function keyPressed(){
-  // p for PAUSE / PLAY
-  if(key == 'p'){
-    if(videoIsPlaying){
-      videoIsPlaying = false;
-      video_input.pause();
-    }
-    else{
-      videoIsPlaying = true;
-      // call link PoseNet here so that it is relinked on the loop and the code can run over and over
-      linkPoseNet();
-      video_input.loop();
-    } 
-  }
-  if(key == 'd' && drawKeypoints_toggle == false){
-    drawKeypoints_toggle = true;
-  }
-  else if(key == 'd' && drawKeypoints_toggle == true){
-    drawKeypoints_toggle = false;
-  }
-
-  // k for key
-  if( key == 'k'){
-    // call find positions to do the calculations for comparison
-    findPositions();
-    displayMinimumStill();
-    // video_input.pause();
-    // videoIsPlaying = false;
-    // display the images
-  }
-
-  // adjust threshold
-  if(keyCode == LEFT_ARROW){
-    threshold -= 50;
-  }
-  if(keyCode == RIGHT_ARROW){
-    threshold += 50;
-  }
-
-  // fullscreen mode f
-  if( key == 'f'){
-    let fs = fullscreen();
-    fullscreen(!fs);
-  }
-}
-
-function detectedPose(results){
-  // we store the pose in the poses global variable
-  poses = results;
 }
 
 // function to draw ellipses over the detected keypoints
@@ -301,8 +293,8 @@ function findLowestDistance(eFD){
 }
 
 function displayMinimumStill(){
-  // cant just draw an image from folder need to preload them in and ref them here
-  // add threshold
+  // cant just draw an image from folder, no need to preload them in and ref them here
+  // add threshold - only draw if the still is similar enough
   if(minimumVal < threshold){
     image(StillImages[minimumStill], 0, 0, width, height);
   }
